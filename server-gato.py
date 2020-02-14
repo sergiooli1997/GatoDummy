23a5cb15c6105bb5a2d62f5c40b645561ffcbfe9
sergiooli1997 <gio.lozol@outlook.com>
2020-02-13 21:44:49 -0600

    Version UDP server

--- server-gato.py
+++ server-gato.py
@@ -7,7 +7,7 @@ import os
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
-buffer_size = 1024
+bufferSize = 1024
 
 
 def horizontal(tablero, n):
@@ -90,104 +90,100 @@ def diagonal(tablero, n):
     return 0
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPServerSocket:
-    TCPServerSocket.bind((HOST, PORT))
-    TCPServerSocket.listen()
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as UDPServerSocket:
+    UDPServerSocket.bind((HOST, PORT))
     print("El servidor TCP está disponible y en espera de solicitudes")
-    Client_conn, Client_addr = TCPServerSocket.accept()
-    with Client_conn:
-        count = 0
-        print("Conectado a", Client_addr)
-        while True:
-            print("Esperando a recibir datos... ")
-            data = Client_conn.recv(buffer_size)
-            dificultad = int.from_bytes(data, byteorder='big')
-            print("Recibido,", dificultad, "   de ", Client_addr)
-            if dificultad == 1:
-                tablero = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
-                while True:
-                    if horizontal(tablero, 3) == 0 and vertical(tablero, 3) == 0 and diagonal(tablero, 3) == 0:
-                        Client_conn.sendall(bytes([0]))
-                    if horizontal(tablero, 3) == 1 or vertical(tablero, 3) == 1 or diagonal(tablero, 3) == 1:
-                        Client_conn.sendall(bytes([1]))
-                        break
-                    if horizontal(tablero, 3) == 2 or vertical(tablero, 3) == 2 or diagonal(tablero, 3) == 2:
-                        Client_conn.sendall(bytes([2]))
-                        break
-                    data = Client_conn.recv(buffer_size)
-                    x = int.from_bytes(data, "big")
-                    data = Client_conn.recv(buffer_size)
-                    y = int.from_bytes(data, "big")
-                    tablero[x][y] = 'X'
-                    print(tablero)
-                    # determinar ganador
-                    if horizontal(tablero, 3) == 0 and vertical(tablero, 3) == 0 and diagonal(tablero, 3) == 0:
-                        Client_conn.sendall(bytes([0]))
-                    if horizontal(tablero, 3) == 1 or vertical(tablero, 3) == 1 or diagonal(tablero, 3) == 1:
-                        Client_conn.sendall(bytes([1]))
-                        break
-                    if horizontal(tablero, 3) == 2 or vertical(tablero, 3) == 2 or diagonal(tablero, 3) == 2:
-                        Client_conn.sendall(bytes([2]))
-                        break
+    count = 0
+    while True:
+        print("Esperando a recibir datos... ")
+        data, address = UDPServerSocket.recvfrom(bufferSize)
+        dificultad = int.from_bytes(data, byteorder='big')
+        print("Recibido,", dificultad, "   de ", address)
+        if dificultad == 1:
+            tablero = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
+            while True:
+                if horizontal(tablero, 3) == 0 and vertical(tablero, 3) == 0 and diagonal(tablero, 3) == 0:
+                    UDPServerSocket.sendto(bytes([0]), address)
+                if horizontal(tablero, 3) == 1 or vertical(tablero, 3) == 1 or diagonal(tablero, 3) == 1:
+                    UDPServerSocket.sendto(bytes([1]), address)
+                    break
+                if horizontal(tablero, 3) == 2 or vertical(tablero, 3) == 2 or diagonal(tablero, 3) == 2:
+                    UDPServerSocket.sendto(bytes([2]), address)
+                    break
+                data, address = UDPServerSocket.recvfrom(bufferSize)
+                x = int.from_bytes(data, "big")
+                data, address = UDPServerSocket.recvfrom(bufferSize)
+                y = int.from_bytes(data, "big")
+                tablero[x][y] = 'X'
+                print(tablero)
+                # determinar ganador
+                if horizontal(tablero, 3) == 0 and vertical(tablero, 3) == 0 and diagonal(tablero, 3) == 0:
+                    UDPServerSocket.sendto(bytes([0]), address)
+                if horizontal(tablero, 3) == 1 or vertical(tablero, 3) == 1 or diagonal(tablero, 3) == 1:
+                    UDPServerSocket.sendto(bytes([1]), address)
+                    break
+                if horizontal(tablero, 3) == 2 or vertical(tablero, 3) == 2 or diagonal(tablero, 3) == 2:
+                    UDPServerSocket.sendto(bytes([2]), address)
+                    break
+                count += 1
+                if count == 9:
+                    break
+                else:
+                    while True:
+                        x_server = random.randint(0, 2)
+                        y_server = random.randint(0, 2)
+                        if tablero[x_server][y_server] == '-':
+                            UDPServerSocket.sendto(bytes([x_server]), address)
+                            UDPServerSocket.sendto(bytes([y_server]), address)
+                            break
+                        else:
+                            print("Ocupado :c")
                     count += 1
-                    if count == 9:
-                        break
-                    else:
-                        while True:
-                            x_server = random.randint(0, 2)
-                            y_server = random.randint(0, 2)
-                            if tablero[x_server][y_server] == '-':
-                                Client_conn.sendall(bytes([x_server]))
-                                Client_conn.sendall(bytes([y_server]))
-                                break
-                            else:
-                                print("Ocupado :c")
-                        count += 1
-                        print("Tiro Servidor")
-                        tablero[x_server][y_server] = 'O'
-                        print(tablero)
-            if dificultad == 2:
-                tablero = [['-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-'],
-                           ['-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-']]
-                while True:
-                    if horizontal(tablero, 5) == 0 and vertical(tablero, 5) == 0 and diagonal(tablero, 5) == 0:
-                        Client_conn.sendall(bytes([0]))
-                    if horizontal(tablero, 5) == 1 or vertical(tablero, 5) == 1 or diagonal(tablero, 5) == 1:
-                        Client_conn.sendall(bytes([1]))
-                        break
-                    if horizontal(tablero, 5) == 2 or vertical(tablero, 5) == 2 or diagonal(tablero, 5) == 2:
-                        Client_conn.sendall(bytes([2]))
-                        break
-                    data = Client_conn.recv(buffer_size)
-                    x = int.from_bytes(data, "big")
-                    data = Client_conn.recv(buffer_size)
-                    y = int.from_bytes(data, "big")
-                    tablero[x][y] = 'X'
+                    print("Tiro Servidor")
+                    tablero[x_server][y_server] = 'O'
                     print(tablero)
-                    # determinar ganador
-                    if horizontal(tablero, 5) == 0 and vertical(tablero, 5) == 0 and diagonal(tablero, 5) == 0:
-                        Client_conn.sendall(bytes([0]))
-                    if horizontal(tablero, 5) == 1 or vertical(tablero, 5) == 1 or diagonal(tablero, 5) == 1:
-                        Client_conn.sendall(bytes([1]))
-                        break
-                    if horizontal(tablero, 5) == 2 or vertical(tablero, 5) == 2 or diagonal(tablero, 5) == 2:
-                        Client_conn.sendall(bytes([2]))
-                        break
+        if dificultad == 2:
+            tablero = [['-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-']]
+            while True:
+                if horizontal(tablero, 5) == 0 and vertical(tablero, 5) == 0 and diagonal(tablero, 5) == 0:
+                    UDPServerSocket.sendto(bytes([0]), address)
+                if horizontal(tablero, 5) == 1 or vertical(tablero, 5) == 1 or diagonal(tablero, 5) == 1:
+                    UDPServerSocket.sendto(bytes([1]), address)
+                    break
+                if horizontal(tablero, 5) == 2 or vertical(tablero, 5) == 2 or diagonal(tablero, 5) == 2:
+                    UDPServerSocket.sendto(bytes([2]), address)
+                    break
+                data, address = UDPServerSocket.recvfrom(bufferSize)
+                x = int.from_bytes(data, "big")
+                data, address = UDPServerSocket.recvfrom(bufferSize)
+                y = int.from_bytes(data, "big")
+                tablero[x][y] = 'X'
+                print(tablero)
+                # determinar ganador
+                if horizontal(tablero, 5) == 0 and vertical(tablero, 5) == 0 and diagonal(tablero, 5) == 0:
+                    UDPServerSocket.sendto(bytes([0]), address)
+                if horizontal(tablero, 5) == 1 or vertical(tablero, 5) == 1 or diagonal(tablero, 5) == 1:
+                    UDPServerSocket.sendto(bytes([1]), address)
+                    break
+                if horizontal(tablero, 5) == 2 or vertical(tablero, 5) == 2 or diagonal(tablero, 5) == 2:
+                    UDPServerSocket.sendto(bytes([2]), address)
+                    break
+                count += 1
+                if count == 25:
+                    break
+                else:
+                    while True:
+                        x_server = random.randint(0, 4)
+                        y_server = random.randint(0, 4)
+                        if tablero[x_server][y_server] == '-':
+                            UDPServerSocket.sendto(bytes([x_server]), address)
+                            UDPServerSocket.sendto(bytes([y_server]), address)
+                            break
+                        else:
+                            print('Ocupado')
                     count += 1
-                    if count == 25:
-                        break
-                    else:
-                        while True:
-                            x_server = random.randint(0, 4)
-                            y_server = random.randint(0, 4)
-                            if tablero[x_server][y_server] == '-':
-                                Client_conn.sendall(bytes([x_server]))
-                                Client_conn.sendall(bytes([y_server]))
-                                break
-                            else:
-                                print('Ocupado')
-                        count += 1
-                        print("Tiro Servidor")
-                        tablero[x_server][y_server] = 'O'
-                        print(tablero)
-            break
+                    print("Tiro Servidor")
+                    tablero[x_server][y_server] = 'O'
+                    print(tablero)
+        break
